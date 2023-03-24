6b037483bd2016c9e89fe2eb09ba6b45abacd54f
ubuntu <ubuntu@localhost.localdomain>
2023-03-24 13:09:51 +0800

    add Last Updated

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,15 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
-  base: '/pour-or-not/'
+  base: '/pour-or-not/',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    lastUpdated: 'Last Updated', // string | boolean
+    // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
+    nextLinks: false,
+    // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
+    prevLinks: false
+  }
 }
